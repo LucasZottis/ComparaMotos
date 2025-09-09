6a9c18f0efdf5cba9e3c28191066ae42e869c146
Lucas Zottis <zottis.lucas@hotmail.com>
2025-09-09 01:07:53 -0300

    Reescrevendo o código

---
+++ src/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'main-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet
+],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss'
+})
+
+export class MainLayoutComponent {
+
+}
